2020cf58d44aa3c269e4e74e492323eec158b7fc
PulinaLi <liyanagepulina10@gmail.com>
2026-08-28 22:28:58 +0530

    005

--- Full_Code/Micromouse/Micromouse.ino
+++ Full_Code/Micromouse/Micromouse.ino
@@ -718,18 +718,22 @@ void loop() {
       LOG_INFO("Obstacle! STATE -> TURN");
     } else {
       // Wall following PD-Controller
-      int d_error = error - prev_error;
-      prev_error = error;
-
-      int steering = 0;
-      // Do not change motor speed if error is 0 (deadband)
-      if (error != 0) {
-        steering = (kp * error) + (kd * d_error);
+      // Wall following Bang-Bang Controller
+      int deadband = 15; // Only react if error is larger than 15mm
+      
+      if (error > deadband) {
+        // Too close to Left Wall (using the robot's specific polarity)
+        left_pwm = 850;
+        right_pwm = 950;
+      } else if (error < -deadband) {
+        // Too close to Right Wall
+        left_pwm = 950;
+        right_pwm = 850;
+      } else {
+        // Centered - Drive straight
+        left_pwm = 950;
+        right_pwm = 950;
       }
-
-      // Fixed steering polarity (was turning into the wrong wall)
-      left_pwm = 950 - steering;
-      right_pwm = 950 + steering;
 
       motor_set_both(left_pwm, right_pwm);
     }
